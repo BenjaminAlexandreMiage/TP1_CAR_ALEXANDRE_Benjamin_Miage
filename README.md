9f676bbde10c2078bcbedcdd58ca10ef571810e7
benjamin alexandre <benjamin.alexandre.etu@univ-lille.fr>
2024-01-13 20:29:59 +0100

    Ajout des cas du login ou password incorrectes et modification du readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-Compte-Rendu du TP1 : 
+*ALEXANDRE Benjamin*
+*MIAGE 1*
+*Groupe 2*
+
+## Compte-Rendu du TP1 : 
+
+---
+
+##  Partie 1 : 
+
+**Le scénario** : *Un utilisateur doit pouvoir se connecter avec un login et un mot de passe, et utiliser la commande quit pour se déconnecter* 
+
+**La conception** : 
+
+### Les variables : 
+
+- *login1* : L'identifiant écrit en brute pour que le client ce connecte.
+- *mdp1* : Le mot de passe en lien avec l'identifiant.
+- *boucle* : boolean qui permet de gérer la boucle infinie du serveur.
+
+### Les événements : 
+
+Le client doit d'abord entrer son login, s'il est correcte il reçoit le code 331, sinon il reçoit le code d'erreur 430.
+
+Ensuite le client doit entrer son mot de passe, s'il est correcte il reçoit le code 230, sinon il reçoit le code 430.
+
+Ensuite, la boucle infinie se lance en attendant une commande du client, si le client envoie la commande  `ftp> quit` alors il reçoit le code 221, sinon pour le moment pour tout autres commandes, il reçoit le code 500 car aucune autre commande n'est reconnue par le serveur.
+
+### Tester le programme :
+
+Il faut ouvrir un premier terminale pour le serveur et un second pour le client.
+
+Pour le serveur, il faut dans un premier temps se rendre dans le dossier où se situe le fichier ServeurFTP.java et lancer la commande : `> javac ServeurFTP.java`  afin de compiler la classe.
+
+Ensuite il faut lancer la commande : `> java ServeurFTP` afin de lancer le programme. 
+
+Du côté du client, il faut lancer la commande : `> ftp localhost 2121` afin de se connecter au serveur (via le port).
+
+Ensuite il ne reste plus qu'à se connecter avec le bon identifiant et le bon mot de passe.
+
+---
--- ServeurFTP.java
+++ ServeurFTP.java
@@ -6,10 +6,12 @@ public class ServeurFTP {
 
     public static void main(String[] args) throws IOException{
 
+        //Création brute d'un login et mot de passe
         String login1 = "Benjamin";
         String mdp1 = "a";
 
-         System.out.print("Serveur prêt à accepter des connexions sur le port 2121");
+        //On affiche que le serveur est prêt (et son port)
+        System.out.print("Serveur prêt à accepter des connexions sur le port 2121");
         
         // On crée le socket du serveur
         ServerSocket serv = new ServerSocket(2121) ;
@@ -21,14 +23,18 @@ public class ServeurFTP {
         InputStream in = s2.getInputStream();
         Scanner scanner = new Scanner(in);
 
-        //On récupère le outputStream
+        //On récupère le outputStream et on envoie le message au client
         OutputStream out = s2.getOutputStream();
         String str1 = "220 Service ready\r\n";
         out.write(str1.getBytes());
 
-        //On attend le prochain message 
+        //On attend le prochain message (l'identifiant)
         String str = scanner.nextLine();
 
+        //Variable pour que le programme fonctionne en continue tan que le client n'a pas fait la commande 'quit' 
+        boolean boucle = true;
+
+        //Si le message est bien un login valide, on envoie que la réponse est valide, sinon on envoie l'erreur
         if(str.equals("USER "+login1)){
             System.out.print("\n");
             System.out.print(str);
@@ -36,25 +42,49 @@ public class ServeurFTP {
             str1 = "331 login is okay\r\n";
             out.write(str1.getBytes());
         }
+        else{
+            str1 = "430 Login invalide\r\n";
+            out.write(str1.getBytes());
+        } 
 
+        //On attend le prochain message (le mot de passe)
         str = scanner.nextLine();
         
+        //Si le message est bien le mot de passe valide, on envoie que la réponse est valide, sinon on envoie l'erreur
         if(str.equals("PASS "+mdp1)){
             System.out.print("\n");
             System.out.print(str);
             out = s2.getOutputStream();
             str1 = "230 mdp is okay\r\n";
             out.write(str1.getBytes());
-
-            str = scanner.nextLine();
         }
+        else{
+            str1 = "430 password invalide\r\n";
+            out.write(str1.getBytes());
+        } 
 
-        while(true){
+        //Si tout est valide, on lance la boucle infinie
+        while(boucle){
+
+            //On attend le prochain message
+            str = scanner.nextLine();
+
+            //Si la commande est 'quit'
             if(str.equals("QUIT")){
+
+                //Message pour le client
+                str1 = "221 deconnexion\r\n";
+                //On envoie le message au client
+                out.write(str1.getBytes());
+                //On affiche sur le serveur que les client c'est déconnecté
+                System.out.print("\n");
                 System.out.print("Client déconnecté");
+                System.out.print("\n");
+                //On arrete la boucle
+                boucle = false;
             }
             else{
-                out = s2.getOutputStream();
+                // Si on recoit une commande inconnue (pour le moment autre que 'quit')
                 str1 = "500 commande non reconnu\r\n";
                 out.write(str1.getBytes());
             }
